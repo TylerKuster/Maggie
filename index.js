f21869c22a84dab282bb67f7b9b73fc050d27753
Kuster Tyler (AA/PJ-COSE3-NA) <Tyler.Kuster@us.bosch.com>
2017-04-10 12:06:44 -0400

    added procfile and index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.post('/hello', funtion(req, res) {
+	res.send('hello world');
+})
+
+// app.post('/webhook/', funtion(req, res) {
+// 	res.send('hello world');
+// })
+
+app.listen(process.env.PORT);
