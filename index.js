0c140892a30f47139bd690b515a8bc5ef482ed48
Kuster Tyler (AA/PJ-COSE3-NA) <Tyler.Kuster@us.bosch.com>
2017-04-10 14:54:32 -0400

    added hook for vehicle-link PUSH API tests

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res) 
 {
-  res.send('hello world')
+  res.send('Maggie is working!')
 })
 
-// app.post('/webhook/', funtion(req, res) {
-// 	res.send('hello world');
-// })
+app.get('/vehicle-link', function (req, res) 
+{
+  res.send('Success!')
+})
+
+app.post('/webhook/', funtion(req, res) {
+	res.send('hello world');
+})
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'))
