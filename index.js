da9fb78050e8d5cdde09fe9e14f7b7bd4583f367
Kuster Tyler (AA/PJ-COSE3-NA) <Tyler.Kuster@us.bosch.com>
2017-04-11 12:17:49 -0400

    rookie mistake, get to post change

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res)
   res.send('Maggie is working!')
 })
 
-app.get('/vehicle-link', function (req, res) 
+app.post('/vehicle-link', function (req, res) 
 {
 	console.log(req);
 
