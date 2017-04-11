d924dc3021b5e385cd91cdbab10a76e17f9412b4
Kuster Tyler (AA/PJ-COSE3-NA) <Tyler.Kuster@us.bosch.com>
2017-04-11 09:16:58 -0400

    added facebook webhook to test connection

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ app.get('/vehicle-link', function (req, res)
   res.send('Success!')
 })
 
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === <SECRET>) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
 // app.post('/webhook/', funtion(req, res) {
 // 	res.send('hello world');
 // })
