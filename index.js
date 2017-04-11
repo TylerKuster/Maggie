4409c45844b8680311b150856acc5c305a09013d
Kuster Tyler (AA/PJ-COSE3-NA) <Tyler.Kuster@us.bosch.com>
2017-04-11 13:44:10 -0400

    trying setting the port manually

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(enforce.HTTPS());
 
 app.use(enforce.HTTPS({ trustProtoHeader: true }))
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (443));
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
